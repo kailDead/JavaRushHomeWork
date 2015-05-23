a825e7e9d4d39e3649d3b709455997f32fa618c4
kailDead <alex12051986@inbox.ru>
2015-05-23 19:33:20 +0600

    Initial commit

---
+++ src/com/javarush/test/level00/lesson03/task01/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.test.level00.lesson03.task01;
+
+/* Лила самая красивая
+Написать программу, выводящую на экран надпись «Лила самая красивая» 5 раз. Каждый раз – с новой строки.
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Лила самая красивая");
+        System.out.println("Лила самая красивая");
+        System.out.println("Лила самая красивая");
+        System.out.println("Лила самая красивая");
+        System.out.println("Лила самая красивая");
+    }
+}
---
+++ src/com/javarush/test/level01/lesson06/task01/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.test.level01.lesson06.task01;
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+         String a="$800/";
+        String b="$1500/";
+        String c="$2200/";
+        String d="$2700/";
+        String e="$3200/";
+        String text1="Я согласен на заплату";
+
+
+
+        System.out.println(text1+" "+a+"месяц"+" "+"в"+"первый"+"год." );
+        System.out.println(text1+" "+b+"месяц"+" "+"в"+"первый"+"год." );
+        System.out.println(text1+" "+c+"месяц"+" "+"в"+"первый"+"год." );
+        System.out.println(text1+" "+d+"месяц"+" "+"в"+"первый"+"год." );
+        System.out.println(text1+" "+e+"месяц"+" "+"в"+"первый"+"год." );
+
+
+
+
+
+
+    }
+}
